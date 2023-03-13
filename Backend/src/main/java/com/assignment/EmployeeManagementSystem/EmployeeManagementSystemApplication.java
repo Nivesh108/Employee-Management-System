360088ccd6e6aa055b184f77061a0a248c5c8701
Nivesh108 <sharmanivesh08@gmail.com>
2023-03-13 21:07:41 +0530

    Trying Exception handling

--- Backend/src/main/java/com/assignment/EmployeeManagementSystem/EmployeeManagementSystemApplication.java
+++ Backend/src/main/java/com/assignment/EmployeeManagementSystem/EmployeeManagementSystemApplication.java
@@ -1,12 +1,20 @@
 package com.assignment.EmployeeManagementSystem;
+import com.assignment.EmployeeManagementSystem.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmployeeManagementSystemApplication {
+public class EmployeeManagementSystemApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementSystemApplication.class, args);
 	}
 
+	@Autowired
+	private EmployeeRepo employeeRepo;
+	@Override
+	public void run(String... args) throws Exception {
+	}
 }
